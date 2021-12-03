213b26ec873a71f89a002d04cb47ff67cc7db7b6
Nathan Daix <46789008+natedx@users.noreply.github.com>
2021-12-03 16:43:20 +0100

    Added state and placeholder

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Person.css"
 
 export default function Person (props : {item: {firstName: String, lastName: String, imageFile: String}}) {
 
     let { item } = props;
+    let [image, setImage] = useState(process.env.PUBLIC_URL + '/images/'+ item.imageFile +'.png')
 
     return(
         <div className={"personContainer"}>
-            <img src={process.env.PUBLIC_URL + '/images/'+ item.imageFile +'.png'} className="App-logo" alt="logo" />
+
+            <img src={image}
+                 className="App-logo" alt="logo"
+                 onError={
+                     () => setImage(process.env.PUBLIC_URL + "/placeholder.jpeg")
+                 }
+            />
+
             <p>{item.firstName} {item.lastName.toUpperCase()}</p>
         </div>
     )
