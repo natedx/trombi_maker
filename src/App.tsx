f9138d49f3847276fbd041c31cfb94804484a925
Nathan Daix <46789008+natedx@users.noreply.github.com>
2021-12-03 15:15:37 +0100

    wllh c'est dégeu

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import names from './names.json'
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+          className="App-header"
+      >
+          <p>Trombi</p>
       </header>
+
+        <div className={"container"}>
+        {
+            names.names.map((item) => {
+
+                return(
+                    <div>
+                        <img src={process.env.PUBLIC_URL + '/images/'+ item.imageFile +'.png'} className="App-logo" alt="logo" />
+                        <p>{item.firstName}, {item.lastName}</p>
+                    </div>
+                )
+            })
+        }
+        </div>
     </div>
   );
 }
